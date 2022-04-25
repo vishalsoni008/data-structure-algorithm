ef37f13e414f6c65f8d888142fe0a6eb7ee8d0e6
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2022-04-25 11:27:56 +0530

    Initial commit with linkedlist

---
+++ src/main/java/linkedlist/LinkedListApp.java
@@ -0,0 +1,43 @@
+package linkedlist;
+
+public class LinkedListApp {
+
+    static Node head;
+    class Node{
+          int data;
+         Node next;
+    }
+    public void insert(int data){
+        Node node = new Node();
+        node.data = data;
+
+        if(head == null){
+            head = node;
+        }
+        else {
+            Node  n =head;
+            while (n.next!=null){
+                n= n.next;
+            }
+            n.next = node;
+        }
+    }
+
+    public void show(){
+        Node node = head;
+        while (node.next!=null){
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+    public static void main(String[] args) {
+        LinkedListApp linkedListApp = new LinkedListApp();
+
+        for(int  i=0; i<=4;i++){
+            linkedListApp.insert(i);
+        }
+
+        linkedListApp.show();
+    }
+}
---
+++ src/main/java/linkedlist/Runner.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.insert(3);
+        linkedList.insert(5);
+        linkedList.insert(7);
+        linkedList.insert(6);
+
+        linkedList.show();
+    }
+}
