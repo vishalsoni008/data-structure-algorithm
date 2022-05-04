c252524cbb43b103a1db83c39f01518387dded46
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2022-05-04 17:13:13 +0530

    added stack implement using linkedlist

---
+++ src/main/java/stack/StackImplementUsingLinkedList.java
@@ -0,0 +1,54 @@
+package stack;
+
+public class StackImplementUsingLinkedList {
+    static Node head;
+
+    class Node{
+        int data;
+        Node next;
+    }
+
+    public void push(int data){
+        Node node = new Node();
+        node.data = data;
+
+        if(head == null){
+            head = node;
+        }
+        else {
+            Node n =head;
+
+            while (n.next!=null){
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+
+    void show(){
+        Node node = head;
+        while (node.next!=null){
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+
+    void peek(){
+        Node node = head;
+        while (node.next!=null){
+            node= node.next;
+        }
+        System.out.println("peek element is "+node.data);
+    }
+
+    public static void main(String[] args) {
+        StackImplementUsingLinkedList stack = new StackImplementUsingLinkedList();
+
+        stack.push(2);
+        stack.push(7);
+        stack.peek();
+        stack.show();
+
+    }
+}
