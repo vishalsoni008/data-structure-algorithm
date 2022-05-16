cc90c325b77f4cf2454c31cbda766a7724473e5a
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2022-05-16 11:24:24 +0530

    added merge and sort two array

---
+++ src/main/java/array/MergeTwoArray.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeTwoArray {
+
+    public static void main(String[] args) {
+        int arr1[] = {1,8,2,5};
+        int arr2[] = {1,9,3,7};
+//
+//        List list = Arrays.asList(arr1);
+//        list.add(arr2);
+//
+//        Object[] ob = list.toArray();
+//        Arrays.sort(ob);
+//        for (int i=0;i< ob.length;i++){
+//            System.out.print(ob[i]+" ");
+//        }
+
+        int[] arr3 = new int[arr1.length+ arr2.length];
+        System.arraycopy(arr1,0,arr3,0,arr1.length);
+        System.arraycopy(arr2,0,arr3,arr1.length,arr2.length);
+        System.out.println(Arrays.toString(arr3));
+        Arrays.sort(arr3);
+        System.out.println(Arrays.toString(arr3));
+    }
+}
