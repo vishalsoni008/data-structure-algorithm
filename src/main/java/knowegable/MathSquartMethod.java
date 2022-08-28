a0a06a8914ecab6c41a520e5f68496ca859e67e9
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2022-08-28 12:38:32 +0530

    correct prime number program

--- src/main/java/knowegable/MathSquartMethod.java
+++ src/main/java/knowegable/MathSquartMethod.java
@@ -1,24 +1,26 @@
 package knowegable;
 
 public class MathSquartMethod {
-    static boolean isPrime(int n){
-        if (n<=1){
-            return false;
-        }
-        for (int  i =2;i<Math.sqrt(n);i++){
-            if (n%i == 0){
-                return false;
-            }
-        }
 
+    public static boolean isPrime(int n){
+        if(n<=1)
+            return false;
+
+        for (int i=2;i<=Math.sqrt(n);i++){
+            if (n%i==0)
+                return false;
+        }
         return true;
     }
+
     public static void main(String[] args) {
-        if (isPrime(4)){
-            System.out.println("this is a prime number");
-        }
-        else {
-            System.out.println("this is not a prime number");
-        }
+        int n = 7;
+
+        if(isPrime(n))
+            System.out.println("this is a prime number "+n);
+
+        else
+            System.out.println("that is not a prime number");
     }
+
 }
