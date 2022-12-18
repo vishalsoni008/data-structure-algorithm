433d56bea164f8aa6563972ab8812c003f83ce31
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2022-12-18 11:24:21 +0530

    added file tree ds

---
+++ src/main/java/tree/binarytree/BinaryTree.java
@@ -0,0 +1,37 @@
+package tree.binarytree;
+
+class Node{
+    int item;
+    Node left;
+    Node right;
+
+    Node(int value){
+        item = value;
+        left = right = null;
+    }
+}
+
+public class BinaryTree {
+    Node root;
+
+    public BinaryTree(int value){
+        root = new Node(value);
+    }
+    public BinaryTree(){
+        root = null;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+
+        binaryTree.root = new Node(1);
+        binaryTree.root.left = new Node(2);
+        binaryTree.root.right = new Node(3);
+
+        System.out.println(binaryTree.root.item);
+        System.out.println(binaryTree.root.left.item);
+        System.out.println(binaryTree.root.right.item);
+
+
+    }
+}
